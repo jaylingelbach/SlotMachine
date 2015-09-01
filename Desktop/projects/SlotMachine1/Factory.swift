e4e2af1c01924c13e29d05006fd345d5756b7305
jaylingelbach <jay.lingelbach@gmail.com>
2015-09-01 15:01:21 -0500

    completed project

---
+++ Desktop/projects/SlotMachine1/Factory.swift
@@ -0,0 +1,8 @@
+//
+//  Factory.swift
+//  SlotMachine
+//
+//  Created by jerkface on 8/30/15.
+//  Copyright (c) 2015 FoMo. All rights reserved.
+//
+
---
+++ Desktop/projects/SlotMachine1/Slot.swift
@@ -0,0 +1,7 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by jerkface on 8/30/15.
+//  Copyright (c) 2015 FoMo. All rights reserved.
+//
